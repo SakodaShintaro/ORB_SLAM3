3d6950cbbc60c507ad25a14a3072b735747124cd
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-04-15 15:44:25 +0900

    Fixed minorly

--- Examples/ROS2/exec.sh
+++ Examples/ROS2/exec.sh
@@ -7,7 +7,7 @@ DIR_NAME=$(readlink -f $(dirname $0))
 cd $DIR_NAME
 
 # setting up environment variables
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:../../ORB_SLAM3/lib:../../../Pangolin/build/"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:../../lib:../../../Pangolin/build/"
 
 # run ORB_SLAM3_ROS2
 ros2 run ORB_SLAM3_ROS2 Mono \
